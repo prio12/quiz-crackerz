1cebc410a688041196897d0a7718ca3b1ea7160c
MRPrio <maksudurprio.mp@gmail.com>
2022-10-12 03:23:18 +0600

    correct ans showed on click eye icon

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./Option.css";
 
-const Option = ({option,id}) => {
-    // console.log(id)
-    return (
-        <div className='option'>
-            <input type="radio" value='option' name={id} /> {option}
-        </div>
-    );
+const Option = ({ option, id }) => {
+  // console.log(id)
+  return (
+    <div className="option">
+     <div>
+     <p>
+        <input className="radio-btn" type="radio" value="option" name={id} />{" "}
+        {option}
+      </p>
+     </div>
+    </div>
+  );
 };
 
 export default Option;
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Option from '../Option/Option';
 import './questions.css'
+import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = ({questionSmaple}) => {
     // console.log(questionSmaple)
     const {id,question,options,correctAnswer} =questionSmaple;
     // console.log(question,options,correctAnswer)
     // console.log(id)
+    const notify = (correctAnswer) => toast(correctAnswer);
+
     return (
-        <div className='questions'>
+        <div className='questions container'>
+            <div className='qs-header'>
             <h5>{question}</h5>
+              <button onClick={() =>notify(correctAnswer)}><EyeIcon className="eye-icon"/></button>
+            </div>
+            <ToastContainer />
             {
                 options.map((option,index) =><Option
                 key={index}
