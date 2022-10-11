20f487d870dcccaf4b6d4133fea6816bc7e0f9b5
MRPrio <maksudurprio.mp@gmail.com>
2022-10-11 18:39:19 +0600

    fav icon added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Web Testy</Navbar.Brand>
+        <Navbar.Brand href="#home"><span className='title'>Web Testy</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Item style={{ textDecoration: 'none', margin:'10px' }} as={Link} to='/'>Topics</Nav.Item>
-            <Nav.Item style={{ textDecoration: 'none', margin:'10px' }} as={Link} to='/statistics'>Statistics</Nav.Item>
-            <Nav.Item style={{ textDecoration: 'none', margin:'10px' }} as={Link} to='/blogs'>Blogs</Nav.Item>
+            <Nav.Item className='route' as={Link} to='/'>Topics</Nav.Item>
+            <Nav.Item className='route' as={Link} to='/statistics'>Statistics</Nav.Item>
+            <Nav.Item className='route' as={Link} to='/blogs'>Blogs</Nav.Item>
             
             
           </Nav>
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import './Main.css'
 
 const Main = () => {
     return (
